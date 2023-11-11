47052fea229b52138a4465707a6faa6a0d49769b
arilandau <ariellelandau360@gmail.com>
2023-11-11 18:33:45 -0500

    add cart component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
@@ -7,18 +6,11 @@ import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent implements OnInit{
   product: Product | undefined
-
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
-  }
 
   constructor(
     private route: ActivatedRoute, 
@@ -30,5 +22,10 @@ export class ProductDetailsComponent implements OnInit{
     const productIdFromRoute = Number(routeParams.get('productId'));
   
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert(`${product.name} has been added to the cart!`);
   }
 }
